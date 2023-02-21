d2cb0200b8def82ab9eafdb05c833fabfd1ad9b0
Mo Prosper <mothepro@live.com>
2023-02-21 01:24:21 -0600

    check multiple paths before giving up

--- index.ts
+++ index.ts
@@ -1,20 +1,37 @@
+/* filenames reddit uses in sorted by highest quality. */
+const potentialFilenames = [
+  'DASH_1040.mp4',
+  'DASH_720.mp4',
+  'DASH_480.mp4',
+  'DASH_360.mp4',
+  'DASH_240.mp4',
+]
 const { hostname, href, pathname } = location
+
 try {
   assert(hostname.endsWith('reddit.com'), `Click again when you're on reddit`)
   assert(pathname.match(/\/r\/.+\/comments\/.+/), `Click again when you have a reddit post open`)
 
   const title = document.querySelector('h1')?.textContent ?? `reddit video ${Date.now}.mp4`
-  const metaEl = assertNotNull(
-    document.querySelector(`meta[property="og:video"]`),
+  const sourceEl = assertNotNull(
+    document.querySelector(`source`),
     'Can not find video path (video might be NSFW)'
   )
   const videoUrl = new URL(
-    assertNotNull(metaEl.getAttribute('content'), 'Path to video is missing')
+    assertNotNull(sourceEl.getAttribute('src'), 'Path to video is missing')
   )
   const [, id] = videoUrl.pathname.split('/')
-  videoUrl.pathname = `/${id}/DASH_720.mp4`
+  let downloadUrl
+  for (const filename of potentialFilenames) {
+    videoUrl.pathname = `/${id}/${filename}`
+    const response = await fetch(videoUrl, { method: 'HEAD' })
+    if (response.ok) {
+      downloadUrl = videoUrl
+      break
+    }
+  }
 
-  download(title, videoUrl)
+  download(title, assertNotNull(downloadUrl, 'Unable to find direct destination of video'))
 } catch (err) {
   assert(err instanceof Error)
   console.error('reddit-video-downloader', err.message)
@@ -26,6 +43,7 @@ try {
 }
 
 // TODO move these to a helper file but that requires a builder to merge all into one file.
+
 /** Assert an expression is true. */
 function assert(expression: unknown, message = 'Assertion Error'): asserts expression {
   if (!expression) throw new Error(message)
